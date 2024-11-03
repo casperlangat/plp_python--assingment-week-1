5fdd51a82875c8d73b3e274a0c0197f1b196779e
langatcasper <langatcasper@gmail.com>
2024-11-03 15:37:52 +0300

    user_input

---
+++ user_input.py
@@ -0,0 +1,11 @@
+# Ask for the user's name
+name = input("Enter your name: ")
+
+# Ask for the user's age
+age = int(input("Enter your age: "))
+
+# Ask for the user's location
+location = input("Enter your Location: ")
+
+# Print out a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
